00f6901e1985bf744cc86be2832538b4253fb16e
Kristen George <kmstrumpfer@yahoo.com>
2025-06-11 19:08:50 -0400

    Finished git commit -m Finished

--- app.js
+++ app.js
@@ -12,17 +12,20 @@ app.route("/employees").get((req, res) => {
     res.send(employees);
 });
 
-app.route("/employees/:id").get((req, res) => {
-    const {id} = req.params;
-    const employee = employees[+id];
-    if(!employees.includes(+id)){
-       return res.status(404);
-    }
-    res.send(employee);
-});
+
 
 app.route("/employees/random").get((req, res) => {
     const randomId  = Math.floor(Math.random()* employees.length);
     const randomEmployee = employees[randomId];
     res.send(randomEmployee);
+});
+
+app.route("/employees/:id").get((req, res) => {
+    const { id } = req.params;
+    
+    const employee = employees.find((e) => e.id === +id);
+    if(!employee){
+       return res.status(404).send("Employee not found");
+    }
+    res.send(employee);
 });
